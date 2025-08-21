f9d69e42a029c0c90dd6d8d8c9b7848098266b91
Vinay Dangodra <vinaydangodra28@gmail.com>
2025-08-21 17:17:32 +0530

    final desktop done

--- src/pages/Home/Contact.jsx
+++ src/pages/Home/Contact.jsx
@@ -8,10 +8,11 @@ export const Contact = () => {
                 <div className='contact-title section-title'>
                   <span style={{fontSize: "3rem"}}>The Nexus of Collaboration</span><br />
                   Contact Me
-                  <form className='contact-form'>
-                    <input type="text" placeholder='Name' className='contact-name'/>
-                    <input type="email" placeholder='Email' className='contact-email'/>
-                    <textarea placeholder='Message' className='contact-message'/>
+                  <form className='contact-form' action="https://getform.io/f/ajjrmvka" method="POST">
+                    <input type="text" name="name" placeholder='Name' className='contact-name'/>
+                    <input type="email" name="email" placeholder='Email' className='contact-email'/>
+                    <textarea name="message" placeholder='Message' className='contact-message'/>
+                    <button type="submit" className='contact-submit'>Send</button>
                   </form>
                 </div>
             </div>
